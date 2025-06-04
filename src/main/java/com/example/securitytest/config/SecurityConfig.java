0d9f55ac48af3a934c5d8ae21c788acfd817849a
hskhsmm <hskhsmm@naver.com>
2025-06-05 02:00:02 +0900

    feat: 커스텀 로그인 설정

--- src/main/java/com/example/securitytest/config/SecurityConfig.java
+++ src/main/java/com/example/securitytest/config/SecurityConfig.java
@@ -28,6 +28,27 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+        http
+                .formLogin((auth) -> auth
+                        // 사용자 정의 로그인 페이지 경로 설정 (GET /login 요청을 처리할 HTML 페이지 필요)
+                        .loginPage("/login")
+
+                        // 로그인 요청을 처리할 URL (POST /loginProc 요청이 들어오면 Spring Security가 자동 처리)
+                        .loginProcessingUrl("/loginProc")
+
+                        // 로그인 관련 리소스는 모두 인증 없이 접근 가능하도록 허용
+                        .permitAll()
+                );
+
+        http
+                .csrf((auth) ->
+                        // CSRF 보호 기능 비활성화
+                        // 보통 REST API, 간단한 테스트용 프로젝트에서는 disable() 처리함
+                        auth.disable()
+                );
+
+
+
         return http.build(); // 최종 SecurityFilterChain 객체 생성
 
 
