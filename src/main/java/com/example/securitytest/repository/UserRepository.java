ce2b452d9cad69452880cde358f2fbd93d7e40ea
hskhsmm <hskhsmm@naver.com>
2025-08-24 17:51:08 +0900

    feat: 권한별 사용자 조회

--- src/main/java/com/example/securitytest/repository/UserRepository.java
+++ src/main/java/com/example/securitytest/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.securitytest.repository;
 
 import com.example.securitytest.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +13,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     // 추가: 사용자명으로 사용자 조회
     Optional<UserEntity> findByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.role = :role")
+    Optional<UserEntity> findByRole(@Param("role") String role);
 
 }
